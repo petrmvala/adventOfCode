73f534647bba3e90aa02ce4de797c953bacb17a7
Petr Vála <petr.m.vala@gmail.com>
2024-12-28 15:54:08 +0100

    feat(2024-24): Second star

--- 2024_24/main.go
+++ 2024_24/main.go
@@ -5,18 +5,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-type Known map[string]int
-type Unknown map[string]Operator
+type Inputs map[string]int
+type Operations map[string]Operator
 
 type Operator struct {
 	a, b, op string
 }
 
-func (o Operator) Operate(known Known) (int, error) {
+func (o Operator) Operate(known Inputs) (int, error) {
 	var (
 		getA, getB int
 		ok         bool
@@ -49,12 +50,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(GetZet(string(f)))
+	inputs, ops := LoadInput(string(f))
+
+	fmt.Println("Analysis:", AnalyzeAdders(ops))
+
+	fmt.Println(GetZet(inputs, ops))
 }
 
-func GetZet(input string) int {
-	known := Known{}
-	unknown := Unknown{}
+func LoadInput(input string) (Inputs, Operations) {
+	inputs := Inputs{}
+	operations := Operations{}
 
 	for _, line := range strings.Split(input, "\n") {
 		if len(line) == 0 {
@@ -66,23 +71,27 @@ func GetZet(input string) int {
 			if err != nil {
 				log.Fatal(err)
 			}
-			known[s[0]] = int(val)
+			inputs[s[0]] = int(val)
 			continue
 		}
 		expression := strings.Split(line, " -> ")
 		out := expression[1]
 		in := strings.Split(expression[0], " ")
-		unknown[out] = Operator{a: in[0], b: in[2], op: in[1]}
+		operations[out] = Operator{a: in[0], b: in[2], op: in[1]}
 	}
 
-	for len(unknown) > 0 {
-		for k, v := range unknown {
-			out, err := v.Operate(known)
+	return inputs, operations
+}
+
+func GetZet(inputs Inputs, operations Operations) int {
+	for len(operations) > 0 {
+		for k, v := range operations {
+			out, err := v.Operate(inputs)
 			if err != nil {
 				continue
 			}
-			known[k] = out
-			delete(unknown, k)
+			inputs[k] = out
+			delete(operations, k)
 			break
 		}
 	}
@@ -93,7 +102,7 @@ func GetZet(input string) int {
 		return zet[:last-address] + bit + zet[last-address+1:]
 	}
 
-	for k, v := range known {
+	for k, v := range inputs {
 		if !strings.HasPrefix(k, "z") {
 			continue
 		}
@@ -109,4 +118,145 @@ func GetZet(input string) int {
 		log.Fatal(err)
 	}
 	return int(res)
+}
+
+func AnalyzeAdders(ops Operations) string {
+	output := []string{}
+	fixes := Operations{
+		// scw XOR gnj got shj, want z07
+		"z07": Operator{a: "scw", b: "gnj", op: "XOR"},
+		"shj": Operator{a: "scw", b: "gnj", op: "AND"},
+		// wkb XOR ngf got , want z16
+		"wkb": Operator{a: "x16", b: "y16", op: "AND"},
+		"tpk": Operator{a: "x16", b: "y16", op: "XOR"},
+		// pvr XOR gvb got pfn, want z23
+		// Finished, CarryOut: z23
+		"z23": Operator{a: "pvr", b: "gvb", op: "XOR"},
+		"pfn": Operator{a: "jwb", b: "hjp", op: "OR"},
+		// vpt XOR hdg got kcd, want z27
+		// gtc OR z27 not found
+		"z27": Operator{a: "vpt", b: "hdg", op: "XOR"},
+		"kcd": Operator{a: "y27", b: "x27", op: "AND"},
+	}
+
+	for out, op := range fixes {
+		ops[out] = op
+		output = append(output, out)
+	}
+	// z00 is a special case
+	// x00 XOR y00 -> z00
+	// x00 AND y00 -> dhb == CarryOut
+	// start from 1 and initialize carry with dhb
+	carry := "dhb"
+	for i := 1; i < 45; i++ {
+		s := strconv.Itoa(i)
+		if i < 10 {
+			s = "0" + s
+		}
+		a := Adder{
+			A:       "x" + s,
+			B:       "y" + s,
+			Sum:     "z" + s,
+			CarryIn: carry,
+		}
+		fmt.Printf("Processing Adder %+v\n", a)
+		a.Validate(ops)
+		if carry != a.CarryIn {
+			fmt.Printf("Adder %+v expected CarryIn = %s\n", a, carry)
+		}
+		carry = a.CarryOut
+		fmt.Printf("Finished, CarryOut: %s\n", a.CarryOut)
+	}
+
+	sort.Strings(output)
+	return strings.Join(output, ",")
+}
+
+type Adder struct {
+	A        string
+	B        string
+	CarryIn  string
+	CarryOut string
+	Sum      string
+}
+
+func (a *Adder) Validate(ops Operations) bool {
+	valid := true
+	AXorB, err := getOpByInputs(ops, a.A, a.B, "XOR")
+	if err != nil {
+		fmt.Printf("%s XOR %s not found\n", a.A, a.B)
+		valid = false
+	}
+	AAndB, err := getOpByInputs(ops, a.A, a.B, "AND")
+	if err != nil {
+		fmt.Printf("%s AND %s not found\n", a.A, a.B)
+		valid = false
+	}
+	if len(a.CarryIn) == 0 {
+		fmt.Println("CarryIn not present, inferring from output")
+		a.CarryIn, err = getOpByIO(ops, AXorB, a.Sum, "XOR")
+		if err != nil {
+			fmt.Printf("%s XOR ? -> %s not found\n", AXorB, a.Sum)
+		}
+	} else {
+		AXorBXorC, err := getOpByInputs(ops, AXorB, a.CarryIn, "XOR")
+		if err != nil {
+			fmt.Printf("%s XOR %s not found\n", AXorB, a.CarryIn)
+			valid = false
+		}
+		if AXorBXorC != a.Sum {
+			fmt.Printf("%s XOR %s got %s, want %s\n", AXorB, a.CarryIn, AXorBXorC, a.Sum)
+			valid = false
+		}
+	}
+	AXorBAndC, err := getOpByInputs(ops, AXorB, a.CarryIn, "AND")
+	if err != nil {
+		fmt.Printf("%s AND %s not found\n", AXorB, a.CarryIn)
+		valid = false
+	}
+	a.CarryOut, err = getOpByInputs(ops, AXorBAndC, AAndB, "OR")
+	if err != nil {
+		fmt.Printf("%s OR %s not found\n", AXorBAndC, AAndB)
+		valid = false
+	}
+	// if AXorBAndCOrAAndB != a.CarryOut {
+	// 	fmt.Printf("%s OR %s got %s, want %s\n", AXorBAndC, AAndB, AXorBAndCOrAAndB, a.CarryOut)
+	// 	valid = false
+	// }
+	return valid
+}
+
+func getOpByInputs(ops Operations, A, B, Op string) (string, error) {
+	if len(A) == 0 || len(B) == 0 {
+		return "", errors.New("invalid input")
+	}
+	for out, op := range ops {
+		if op.op != Op {
+			continue
+		}
+		if (op.a == A && op.b == B) || (op.a == B && op.b == A) {
+			return out, nil
+		}
+	}
+	return "", errors.New("operation not found")
+}
+
+func getOpByIO(ops Operations, A, Out, Op string) (string, error) {
+	if len(A) == 0 || len(Op) == 0 {
+		return "", errors.New("invalid input")
+	}
+	op, ok := ops[Out]
+	if !ok {
+		return "", errors.New("operation not found")
+	}
+	if op.op != Op {
+		return "", errors.New("operation not found")
+	}
+	if op.a != A && op.b != A || op.a == op.b {
+		return "", errors.New("operation not found")
+	}
+	if op.a == A {
+		return op.b, nil
+	}
+	return op.a, nil
 }
--- 2024_24/main_test.go
+++ 2024_24/main_test.go
@@ -17,9 +17,29 @@ x02 OR y02 -> z02
 `
 
 func TestGetZet(t *testing.T) {
-	got := GetZet(inputSmall)
+	inputs, ops := LoadInput(inputSmall)
+	got := GetZet(inputs, ops)
 	want := 4
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestGetOpByInput(t *testing.T) {
+	_, ops := LoadInput(inputSmall)
+	got, err := getOpByInputs(ops, "x00", "y00", "AND")
+	if err != nil {
+		t.Errorf("expected no error, got %q", err)
+	}
+	gotRev, err := getOpByInputs(ops, "y00", "x00", "AND")
+	if err != nil {
+		t.Errorf("expected no error, got %q", err)
+	}
+	want := "z00"
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+	if gotRev != want {
+		t.Errorf("got %s, want %s", gotRev, want)
 	}
 }
